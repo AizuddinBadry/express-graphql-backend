97698474bfcbadaacb7a865c2ab886bd5cb90806
Aizuddin Badry <aizuddinbadry@outlook.com>
2021-12-31 16:38:30 -0800

    Init objection

---
+++ src/db/connection.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const { Model } = require("objection");
+
+const knex = require("knex");
+
+const knexfile = require("./knexfile");
+
+const env = process.env.NODE_ENV || "development";
+const configOptions = knexfile[env];
+const query = knex(configOptions);
+Model.knex(query);
+
+if (process.env.APP_ENV === "dev" || process.env.APP_ENV === "staging") {
+  query.on("query", function (queryData) {
+    console.log(
+      `[${moment(Date.now()).format("h:mm:ss")}] :`,
+      queryData.sql,
+      queryData.sql === "BEGIN;" || queryData.sql === "COMMIT;"
+        ? ""
+        : queryData.bindings
+    );
+  });
+}
+
+module.exports = query;
